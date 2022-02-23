c4e54894393e57802dd026f64d082f9f80877c72
DSteph <31934098+DSteph@users.noreply.github.com>
2022-02-23 21:21:56 +0100

    Face+body
    Verification de visage ok mais détection multiple, body implémenté mais pas trouvé dans la vidéo de ref

---
+++ face_checker.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+def cycle_face(frame):
+    faces_tab = []
+    # On ajoute si on détecte un visage de face du pattern "default"
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces_tab.append(face_cascade.detectMultiScale(frame, 1.15, 4))
+    # On ajoute si on détecte un visage de face du pattern "alt", et ainsi de suite avec tous les patterns de visage
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+    faces_tab.append(face_cascade.detectMultiScale(frame, 1.15, 4))
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+    faces_tab.append(face_cascade.detectMultiScale(frame, 1.15, 4))
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt_tree.xml')
+    faces_tab.append(face_cascade.detectMultiScale(frame, 1.15, 4))
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+    faces_tab.append(face_cascade.detectMultiScale(frame, 1.15, 4))
+    return faces_tab
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
 import sys
+
+import face_checker
 import upload
 import image_transformer
 import numpy as np
 #import matplotlib.pyplot as plt
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 
 upload_checker = upload.UploadChecker()
 img_trs = image_transformer.ImageTransformer()
@@ -28,12 +28,25 @@ if is_video:
         ret, frame = video.read()
         if ret:
             gray = img_trs.toGray(frame)
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+            #On va récupérer tous les pattern de visage dont on dispose
+            faces_tab = face_checker.cycle_face(gray)
+            for faces_pattern in faces_tab:
+                for (x, y, w, h) in faces_pattern:
+                    cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+                    roi_gray = gray[y:y + h, x:x + w]
+                    roi_color = frame[y:y + h, x:x + w]
+                    #On ecrit ce qui est détecté
+                    cv2.putText(frame, "Visage", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+            #De même avec les corps
+            body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+            bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in bodies:
+                cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 255, 0), 2)
                 roi_gray = gray[y:y + h, x:x + w]
                 roi_color = frame[y:y + h, x:x + w]
-            cv2.imshow('Frame',frame)
+                # On ecrit ce qui est détecté
+                cv2.putText(frame, "Corps", (x+x, y+y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.imshow('Frame', frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
